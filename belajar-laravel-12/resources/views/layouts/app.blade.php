cb4ff0c9a629c1ace5a65ed76471ed4d7de972ae
atiohaidar <66902140+atiohaidar@users.noreply.github.com>
2025-04-09 06:59:43 +0000

    ini tampilannya udah mending

--- belajar-laravel-12/resources/views/layouts/app.blade.php
+++ belajar-laravel-12/resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Laravel 12 API')</title>
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
